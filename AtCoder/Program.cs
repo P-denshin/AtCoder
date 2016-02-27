b3f930c32c27aa7a9184ccc0527eb05aacdd4360
Denshin <zaw37875@gmail.com>
2016-02-28 00:43:43 +0900

    公開

---
+++ AtCoder/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder {
+    class Program {
+        static void Main(string[] args) {
+            Scanner sc = new Scanner(Console.In);
+
+        }
+
+        #region テンプレ
+
+        class Scanner {
+            TextReader tr;
+            String[] terms;
+            int i;
+
+            public int NextInt() {
+                if (terms == null || i == terms.Count()) {
+                    terms = tr.ReadLine().Split(' ');
+                    i = 0;
+                }
+
+                int n;
+                if (int.TryParse(terms[i], out n)) {
+                    i++;
+                    return n;
+                } else {
+                    i++;
+                    return NextInt();
+                }
+            }
+
+            public double NextDouble() {
+                if (terms == null || i == terms.Count()) {
+                    terms = tr.ReadLine().Split(' ');
+                    i = 0;
+                }
+
+                double n;
+                if (double.TryParse(terms[i], out n)) {
+                    i++;
+                    return n;
+                } else {
+                    i++;
+                    return NextDouble();
+                }
+            }
+
+            public String NextString() {
+                if (terms == null || i == terms.Count()) {
+                    terms = tr.ReadLine().Split(' ');
+                    i = 0;
+                }
+
+                if (terms[i] == "") {
+                    i++;
+                    return NextString();
+                } else {
+                    return terms[i++];
+                }
+            }
+
+            public Scanner(TextReader tr) {
+                terms = null;
+                this.tr = tr;
+                i = 0;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AtCoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("AtCoder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtCoder")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("399ce3a6-3c17-42f2-849e-96834926add5")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
